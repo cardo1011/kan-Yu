e503081b5afabf54389951d0c5ef6d51b2e3fdba
cardo1011 <ricardomunoz4250@gmail.com>
2026-02-11 15:08:40 -0800

    set up most of the file structure. Started implementing HTML files and some styling, but i need to plan a bit better how to make the HTML that I want to implement into the project. Probably just need to simplify it all with basic html and basic styling just to get the MVP

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+const userRouter = require("./routes/users.js");
+
+app.use(express.static("public"));
+app.use("/users", userRouter);
+
+app.listen(3000);
