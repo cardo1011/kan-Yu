11437ea431351779b45b6eca504011f9ac72ba84
cardo1011 <ricardomunoz4250@gmail.com>
2026-04-03 14:40:52 -0700

    set up the register form for new users. Need to implement the form to POST the submitted data to the DB

--- app.js
+++ app.js
@@ -2,14 +2,16 @@ const express = require("express");
 const app = express();
 
 app.set("view engine", "ejs");
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.render("index");
 });
 
-const userRouter = require("./routes/users.js");
+//required for req.body to be accessible to for any routes
+app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static("public"));
+const userRouter = require("./routes/users.js");
 app.use("/users", userRouter);
 
 app.listen(3000);
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,26 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.render("index.ejs");
+router.get("/register", (req, res) => {
+  res.render("newUserForm.ejs");
 });
 
-router.get("/new", (req, res) => {
-  res.send("User New Form");
+router.post("/register", (req, res) => {
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const email = req.body.email;
+  const password = req.body.password;
+
+  console.log(req.body);
+});
+
+router.get("/login", (req, res) => {
+  res.send("Waiting for login form to be implemented");
+});
+
+router.get("/profile", (req, res) => {
+  res.send("Waiting for profile ejs file to be implemented");
+  // res.render("profile.ejs");
 });
 
 module.exports = router;
